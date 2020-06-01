9592d1d058ec55ca4dc5cf5ee4e2d431810608e3
Popeskul Pavlo <peterjj91@gmail.com>
2020-06-01 11:33:54 +0300

    Updates

---
+++ precache-manifest.70f967b20c4d8fce49171aec6810f082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1285e5961346cfe9ac45134f126934",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "b5e0d93a62372287519b",
+    "url": "/contact-manager/static/css/main.2ab93896.chunk.css"
+  },
+  {
+    "revision": "8ce0ec9da4a13f70f70b",
+    "url": "/contact-manager/static/js/2.3d62d55d.chunk.js"
+  },
+  {
+    "revision": "e0447d07c581cfec01b69ae89d5eb5e8",
+    "url": "/contact-manager/static/js/2.3d62d55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e0d93a62372287519b",
+    "url": "/contact-manager/static/js/main.525e8cfc.chunk.js"
+  },
+  {
+    "revision": "4cdc90b35cd2e7a7a661",
+    "url": "/contact-manager/static/js/runtime-main.7f261af7.js"
+  }
+]);
